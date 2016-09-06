e0131045615df2a1f2110108c0f805f68a48d56b
Viquoc Quan <vtquan@outlook.com>
2016-09-06 11:47:50 -0400

    Fix project configuration for NUNIT
    Add basic tests checking that Index and About functions exist in HomeController

---
+++ jubilant/src/jubilant.UnitTests/HomeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using jubilant.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jubilant.UnitTests
+{
+    [TestFixture]
+    class HomeControllerTests
+    {
+        [Test]
+        public void CheckIndexExists()
+        {
+            var sut = new HomeController();
+
+            var result = sut.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+        [Test]
+        public void CheckAboutExists()
+        {
+            var sut = new HomeController();
+
+            var result = sut.About() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
